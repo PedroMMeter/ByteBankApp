cca02974bb86f5e7b237ff6253e79940db8110ef
PedroMMeter <pedromoraesmeter@gmail.com>
2021-09-22 21:43:00 -0300

    Class Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,67 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(ByteBankApp());
 
+const darkGreen = Color(0xFF1B5E20);
+
 class ByteBankApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Column(
-          children: [
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              width: 100,
-              height: 120,
-              color: Colors.green,
+      theme: ThemeData(
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+          ),
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(secondary: Colors.blueAccent[900])),
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: darkGreen,
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 150,
+              height: 100,
+              padding: const EdgeInsets.all(10.0),
+              color: darkGreen,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(Icons.people),
-                  Text('Contatos'),
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    'Contatos',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
